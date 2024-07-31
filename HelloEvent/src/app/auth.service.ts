c8c89a297ad80290f07ec5e298321ea587649fee
Reda Ouadi <redaouadi34@gmail.com>
2024-07-31 16:34:05 +0100

    Ajouter interceptor et service de login

---
+++ HelloEvent/src/app/auth.service.ts
@@ -0,0 +1,49 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  private apiUrl = 'http://localhost:8082/api/auth';
+
+  constructor(private http: HttpClient) { }
+
+  private getHeaders() {
+    const token = localStorage.getItem('token');
+    return new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${token}`
+    });
+  }
+
+  login(email: string, password: string): Observable<{ token: string, role: string }> {
+    return this.http.post<{ token: string, role: string }>(`${this.apiUrl}/authenticate`, { email, password }).pipe(
+      tap(response => {
+        if (response.token) {
+          localStorage.setItem('token', response.token);
+          localStorage.setItem('userRole', response.role);
+        }
+      })
+    );
+  }
+
+  register(user: any): Observable<any> {
+    return this.http.post(`${this.apiUrl}/register`, user);
+  }
+
+  logout() {
+    localStorage.removeItem('token');
+    localStorage.removeItem('userRole');
+  }
+
+  isLoggedIn(): boolean {
+    return !!localStorage.getItem('token');
+  }
+
+  getToken(): string | null {
+    return localStorage.getItem('token');
+  }
+}
